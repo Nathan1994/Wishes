6a542a99915c38c2558686d9e90edc10dc906905
Li Zhenliang <coldlefthand@gmail.com>
2014-06-13 19:45:03 +0800

    sign in error

---
+++ struts/WebRoot/js/InputCheck.js
@@ -0,0 +1,47 @@
+function validate_required(field,alerttxt)
+{
+with (field)
+  {
+  if (value==null||value=="")
+    {alert(alerttxt);return false}
+  else {return true}
+  }
+}
+
+function validate_form_required(thisform)
+{
+with (thisform)
+  {
+  if (validate_required(user.email,"Email must be filled out!")==false)
+    {email.focus();return false}
+  }
+}
+
+
+
+
+function validate_email(field,alerttxt)
+{
+with (field)
+{
+apos=value.indexOf("@")
+dotpos=value.lastIndexOf(".")
+if (apos<1||dotpos-apos<2) {
+  	alert(alerttxt);
+  	return false
+  }
+else {
+	return true
+	}
+  }
+}
+
+function validate_form_email(thisform)
+{
+with (thisform)
+{
+if (
+	validate_email(useremail,"请输入正确的邮箱格式!")==false)
+  {email.focus();return false}
+}
+}
