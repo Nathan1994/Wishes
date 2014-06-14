2ff8ef3d41c5be5e48d6b168757b10c5c1dedf3e
Nathan <shaozeman@qq.com>
2014-06-14 10:28:15 +0800

    add action to db

--- struts/src/com/struts2/path/action/IndexAction.java
+++ struts/src/com/struts2/path/action/IndexAction.java
@@ -1,6 +1,7 @@
 package com.struts2.path.action;
 
 import com.opensymphony.xwork2.ActionSupport;
+import com.struts2.user.model.Operation;
 import com.struts2.user.model.User;
 import com.struts2.user.dto.UserDTO;
 
@@ -11,10 +12,13 @@ public class IndexAction extends ActionSupport{
 	private UserDTO userDTO=new UserDTO();
 	
 	public String add() {
-		user.setEmail(userDTO.getEmail());
-		user.setPassword(userDTO.getPassword());
+		user.setName(userDTO.getEmail());
+//		user.setPassword(userDTO.getPassword());
+		user.setId(1);
 		System.out.println("user=" + user.getEmail());
 		System.out.println("password=" + user.getPassword());
+		Operation op = new Operation();
+		op.dataSave(user);
 		return SUCCESS;
 	}
 	public String signIn(){
